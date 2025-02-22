0ab2de8a392b3ca38f7e293fd7061344109522db
ds797 <73960745+ds797@users.noreply.github.com>
2025-02-22 13:27:54 -0800

    Feat: Create `dir` class

---
+++ dir.h
@@ -0,0 +1,33 @@
+#include <filesystem>
+#include <iostream>
+#include <string>
+#include <vector>
+
+namespace fs = std::filesystem;
+
+class dir {
+public:
+	static fs::path get_cwd() {
+		return fs::current_path();
+	}
+	static std::vector<fs::path> list_directories(const fs::path& path) {
+		std::vector<fs::path> directories;
+
+		try {
+			if (!fs::exists(path) || !fs::is_directory(path)) {
+				std::cerr << path << " is not a valid directory or does not exist.\n";
+			}
+
+			for (const fs::directory_entry& entry : fs::directory_iterator(path)) {
+				if (fs::is_directory(entry.status())) {
+					directories.push_back(entry);
+				}
+			}
+		} catch (const fs::filesystem_error& e) {
+			std::cerr << "Error reading directory: " << e.what() << "\n";
+		}
+
+		// Uses move semantics, so it's plenty fast
+		return directories;
+	}
+};
