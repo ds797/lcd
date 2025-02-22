8b3476135045881153248ce9460a3d824c493ded
ds797 <73960745+ds797@users.noreply.github.com>
2025-02-22 13:41:16 -0800

    Feat: Create `color` class

---
+++ color.cpp
@@ -0,0 +1,8 @@
+#include "color.h"
+
+const int color::fg::black = 30;
+const int color::fg::white = 37;
+
+const int color::bg::transparent = -1;
+const int color::bg::black = 40;
+const int color::bg::white = 47;
---
+++ color.h
@@ -0,0 +1,19 @@
+#ifndef COLOR_H
+#define COLOR_H
+
+namespace color {
+	class fg {
+	public:
+		static const int black;
+		static const int white;
+	};
+
+	class bg {
+	public:
+		static const int transparent;
+		static const int black;
+		static const int white;
+	};
+}
+
+#endif
